0a1eace57d09004f7725e6ab8354971d81a83165
Eugene Pasko <eugene.pasko@alnicko.com>
2021-08-26 17:23:12 +0300

    UntilDateMod change: group usage went not as planned. Swapped with hardcoded values

--- until_date.py
+++ until_date.py
@@ -1,5 +1,9 @@
+# requires apscheduler
+
 import datetime
 import logging
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from .. import loader, utils
 
@@ -9,34 +13,24 @@ logger = logging.getLogger(__name__)
 class UntilDateMod(loader.Module):
     """Tells you how much time left before an event\nMade with love by @Art3sius"""
     strings = {
-        'name': 'UntilDate',
-        'target_time': 'Input time to wait for',
-        'time_left': 'До апокаліпсису лишилось {} годин, {} хвилин і {} секунд',
-        'no_time': 'Time was not set',
-        'bad_time': 'Event has already passed, wtf?'
+        'name': 'UntilDate'
     }
 
-    def __init__(self):
-        self.config = loader.ModuleConfig("TARGET_TIME", None, lambda m: self.strings('target_time', m))
+    async def client_ready(self, client, db):
+        self.client = client
 
-    @loader.unrestricted
-    @loader.ratelimit
-    async def когдаcmd(self, message):
-        """Tells you how soon the scheduled event or [input_date] will happen"""
-        args = utils.get_args(message)
-        if not args:
-            date_time = self.config['TARGET_TIME']
-            if not date_time:
-                await utils.answer(message, self.strings('no_time', message))
-                return
-        else:
-            date_time = ' '.join(args)
+        scheduler = AsyncIOScheduler()
+        scheduler.add_job(self.checks, 'interval', seconds=1)
+        scheduler.start()
 
+    async def checks(self):
         now = datetime.datetime.now()
-        target = datetime.datetime.strptime(date_time, '%d/%m/%Y %H:%M')
+        target = datetime.datetime(2021, 8, 31, 14)
         res = target - now
         if res.days < 0 or res.seconds < 0:
-            await utils.answer(message, self.strings('bad_time', message))
             return
         res_string = f'{str(res.days * 24 + res.seconds // 3600)}:{str(res.seconds % 3600 // 60)}:{str(res.seconds % 60)}'
-        await utils.answer(message, 'До апокаліпсису лишилось: ' + res_string)
+        try:
+            await self.client.edit_message(1588152056, 33404, '<i>До апокаліпсису лишилось: ' + res_string + '</i>')
+        except:
+            pass
